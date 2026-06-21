e39ad6a264a9a6ce6793bfd530d57be991f54b83
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-06-22 00:12:49 +0500

    feat(auth): implement passport local strategy

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require("express");
+
 const app = express();
 const session = require("express-session");
 const path = require("node:path");
 const passport = require("passport");
+require("./config/passport");
 const indexRouter = require("./routes/index");
+const authRouter = require("./routes/authRouter");
+const { pool } = require("./model/dbConnection");
+const pgSession = require("connect-pg-simple")(session);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: true,
+    store: new pgSession({
+      pool,
+      createTableIfMissing: true,
+    }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, //cookie expires after one day
+  }),
+);
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.session)
+  next();
+});
+
+app.use("/auth", authRouter);
 app.use("/", indexRouter);
 
 app.use((error, req, res, next) => {
