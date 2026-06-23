5af2ab513c59c6b3a4e7b7b4967d6ca2bc2680df
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-06-23 18:35:55 +0500

    fix: change host to listen to all connections

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.use((error, req, res, next) => {
   console.log(error);
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log(`Server is listening at http://localhost:3000`);
 });
